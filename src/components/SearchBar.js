ee0216a2de8b0be9fe642728a271647b1942b1c5
JT9 <caleb.tsosie@asu.edu>
2019-04-26 19:21:54 -0700

    Handling Form Submittal
    Disabling default behavior when pressing enter on the keyboard where the user input refreshes the page.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,22 @@ import React from 'react';
 //Class based component due to user input
 class SearchBar extends React.Component {
     state = { term: ''};
+
+    onFormSubmit(event) {
+        //Prevents browser from submitted the form when pressing enter
+        event.preventDefault();
+        //console.log(this.state.term);
+    }
     render() {
         return ( 
         <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                     <label>Image Search</label>
                     <input 
                         type="text" 
-                        value={this.state.term }
-                        onChange={(e) => this.setState({ term: e.target.value })} 
+                        value={this.state.term}
+                        onChange={(e) => this.setState({ term: e.target.value })}
                     />
                 </div>
             </form>
