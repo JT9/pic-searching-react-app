11659807730e1c60fc26a0bb734fd3ffc91767be
JT9 <caleb.tsosie@asu.edu>
2019-04-26 20:40:35 -0700

    Updated Async Wait
    Using Async AWait instead of the previous one using a promise

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-
     //callback function
-    onSearchSubmit(term) {
-        //console.log(term);
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: 'Client-ID 1e1f62fa1554eeccce44fb897964111d8d58e44a47fa4c576b2a9b1ec30f190f'
+    async onSearchSubmit(term) {
+        const response = await axios
+            .get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID 1e1f62fa1554eeccce44fb897964111d8d58e44a47fa4c576b2a9b1ec30f190f'
             }
-        }).then(response => {
-            console.log(response.data.results);
         });
+        console.log(response.data.results);
     }
 
     render(){
