ff16d35e471dd105f7510500ca364cf48054c559
JT9 <caleb.tsosie@asu.edu>
2019-04-26 18:31:18 -0700

    Created SearchBar Component
    Added SearchBar component to App.js and preparing overall project structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Class based component due to user input
+class SearchBar extends React.Component {
+    render() {
+        return ( 
+        <div>
+            <form>
+                <input type="text" />
+            </form>
+        </div>)
+        ;
+    }
+}
+
+export default SearchBar;
