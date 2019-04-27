696fe63907ed3bb6c0f05437540ecf5d0c49d806
JT9 <caleb.tsosie@asu.edu>
2019-04-26 20:28:10 -0700

    Viewing Request Results
    Setup Unsplash API search using Axios and you can view the results in the network section of developer tools

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,12 @@ class App extends React.Component {
     //callback function
     onSearchSubmit(term) {
         //console.log(term);
-        
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 1e1f62fa1554eeccce44fb897964111d8d58e44a47fa4c576b2a9b1ec30f190f'
+            }
+        });
     }
 
     render(){
