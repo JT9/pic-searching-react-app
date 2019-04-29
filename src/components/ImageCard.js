1d781f13114b15f48a175fd280a75cc00964a677
JT9 <caleb.tsosie@asu.edu>
2019-04-29 15:41:58 -0700

    Accessing Image Height
    Trying to figure out a way to access the image height to help with displaying the images correctly

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,22 @@
-//Purpose to show 1 image by itself
-
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef);
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
         const { description, urls } = this.props.image;
         return (
             <div>
-                <img alt={description} src ={urls.regular} />
+                <img ref={this.imageRef} alt={description} src ={urls.regular} />
             </div>
         );
     }
